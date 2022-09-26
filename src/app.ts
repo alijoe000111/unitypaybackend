a4cd399bf7b108447e4f2ecf2230498edd7a78d9
SirYusluv <olanrewajuyusuf379@gmail.com>
2022-09-26 04:11:52 +0100

    Added netlify host to allowed origin

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,10 @@ import TransactionRouter from "./routes/transaction";
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000, https://orbitpay.netlify.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
