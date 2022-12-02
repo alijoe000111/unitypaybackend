2697cad1a4a9f8ebe06958819690bdc13fde3a57
forstuffs <stuffforstuffss@gmail.com>
2022-12-02 02:44:26 +0100

    console log origin

--- app/app.js
+++ app/app.js
@@ -45,6 +45,7 @@ app.use((req, res, next) => {
         "https://unitypaybank.com",
     ];
     const origin = req.headers.origin;
+    console.log(origin);
     res.setHeader("Access-Control-Allow-Origin", allowedOrigin.includes(origin) ? origin : allowedOrigin[1]);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
